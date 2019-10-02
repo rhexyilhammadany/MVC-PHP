ad142e426c38e2f268c50c1b721c74528db5c06d
rhexy ilham madany <rehexy_173040125@mail.unpas.ac.id>
2019-10-02 23:29:00 +0700

    menambahkan routing pada mvc

--- app/core/App.php
+++ app/core/App.php
@@ -1,7 +1,21 @@
 <?php 
 
 class App{
-	public function __contruct(){
-		echo 'OK';
+	public function __construct()
+	{
+		$url = $this->parseURL();
+		var_dump($url);
 	}
+
+	public function parseURL()
+	{
+		if(isset($_GET['url'])){
+			$url = rtrim($_GET['url'],'/');
+			// membersihkan url yang ngaco
+			$url = filter_var($url,FILTER_SANITIZE_URL);
+			// memecah url 
+			$url = explode ('/', $url);
+		    return $url;
+	 }
+   }
 }
