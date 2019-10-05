847be01d556b477b88be034158e60bf0f8f39288
Rhexy Ilham Madany <55421762+rhexyilhammadany@users.noreply.github.com>
2019-10-06 00:10:15 +0700

    Update Home.php

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -4,7 +4,7 @@ class Home {
 	public function index($nama = 'Rhexy' , $pekerjaan = 'mahasiswa'){
 		echo "Hallo , nama saya $nama , Saya adalah seorang $pekerjaan";
 	}
-	public function page();
+	public function page()
 	{
 		echo 'About/page';
 	}
