cc80cabb4ff0b604676d03580cfe7ece118191d0
rhexy ilham madany <rehexy_173040125@mail.unpas.ac.id>
2019-10-06 01:13:41 +0700

    mengupdate controller home.php

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,7 +1,15 @@
 <?php 
 
+<<<<<<< HEAD
 class Home extends Controller {
 	public function index()
+=======
+class Home {
+	public function index($nama = 'Rhexy' , $pekerjaan = 'mahasiswa'){
+		echo "Hallo , nama saya $nama , Saya adalah seorang $pekerjaan";
+	}
+	public function page()
+>>>>>>> 847be01d556b477b88be034158e60bf0f8f39288
 	{
 		$data['judul'] = 'Home';
 		$this->view('templates/header', $data);
