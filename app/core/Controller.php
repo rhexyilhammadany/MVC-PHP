69655459e432dc3e75efb7237d9030f58f58eba8
rhexy ilham madany <rehexy_173040125@mail.unpas.ac.id>
2019-10-06 03:48:54 +0700

    menambahkan model

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,10 +1,17 @@
 <?php 
 
 class Controller{
+	// mengelola  view
 	public function view($view, $data = [])
 	{
 		require_once '../app/views/' . $view . '.php';
 	}
 	
+	// mengelola model
+	public function model($model)
+	{
+		require_once '../app/models/'. $model . '.php';
+		return new $model;
+	}
 }
 
---
+++ app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php 
+
+class User_model{
+	private $nama = 'Rhexy Ilham';
+
+	public function getUser()
+	{
+		return $this->nama;
+	}
+}
