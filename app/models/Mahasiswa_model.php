9279ee0a0b7a388ed1e6322b31e263f5ffa810bf
rhexy ilham madany <rehexy_173040125@mail.unpas.ac.id>
2019-10-06 19:37:02 +0700

    menambahkan fitur delete

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -50,4 +50,33 @@ class Mahasiswa_model
 
         return $this->db->affected_rows();
     }
+      public function ubahDataMahasiswa($data)
+    {
+        $query = "UPDATE mahasiswa SET
+                  nama = :nama,
+                  nrp = :nrp,
+                  email = :email,
+                  jurusan = :jurusan
+                  WHERE id = :id";
+        $this->db->query($query);
+
+        $this->db->bind('id', $data['id']);
+        $this->db->bind('nama', $data['nama']);
+        $this->db->bind('nrp', $data['nrp']);
+        $this->db->bind('email', $data['email']);
+        $this->db->bind('jurusan', $data['jurusan']);
+
+        $this->db->execute();
+
+        return $this->db->affected_rows();
+    }
+
+    public function cariDataMahasiswa()
+    {
+        $keyword = $_POST['keyword'];
+        $query = "SELECT * FROM mahasiswa WHERE nama LIKE :keyword";
+        $this->db->query($query);
+        $this->db->bind('keyword', "%$keyword%");
+        return $this->db->resultSet();
+    }
 }
